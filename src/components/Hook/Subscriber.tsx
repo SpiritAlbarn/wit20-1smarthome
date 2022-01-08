f02b79ee2f79b0bedf77a06592676861b39470bc
SpiritAlbarn <spirit.zat@gmail.com>
2022-01-08 21:13:06 +0100

    wip

--- src/components/Hook/Subscriber.tsx
+++ src/components/Hook/Subscriber.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useContext } from 'react';
 import { Card, Form, Input, Row, Col, Button, Select } from 'antd';
 import { QosOption } from './HookMqtt';
+import { clientOptions } from '../env/clientOptions';
 
 type Props = {
     sub: (subscription: any) => void;
@@ -26,36 +27,45 @@ export const Subscriber: FunctionComponent<Props> = ({ sub, unSub, showUnsub }) 
         unSub(values);
     };
 
-    const SubForm = (
-        <Form layout="vertical" name="basic" form={form} initialValues={record} onFinish={onFinish}>
-            <Row gutter={20}>
-                <Col span={12}>
-                    <Form.Item label="Topic" name="topic">
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item label="QoS" name="qos">
-                        <Select options={qosOptions} />
-                    </Form.Item>
-                </Col>
-                <Col span={8} offset={16} style={{ textAlign: 'right' }}>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Subscribe
-                        </Button>
-                        {showUnsub ? (
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            <Button type="danger" style={{ marginLeft: '10px' }} onClick={handleUnsub}>
-                                Unsubscribe
-                            </Button>
-                        ) : null}
-                    </Form.Item>
-                </Col>
-            </Row>
-        </Form>
+    return (
+        <div className="flex flex-col w-full items-center text-gray-800">
+            <Card title="Subscriber">
+                <Form layout="vertical" name="basic" form={form} initialValues={record} onFinish={onFinish}>
+                    <Row gutter={20}>
+                        <Col span={12}>
+                            <Form.Item label="Topic" name="topic">
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item label="QoS" name="qos">
+                                <Select options={qosOptions} />
+                            </Form.Item>
+                        </Col>
+                        <Col span={8} offset={16} style={{ textAlign: 'right' }}>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Subscribe
+                                </Button>
+                                {showUnsub ? (
+                                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                    // @ts-ignore
+                                    <Button type="danger" style={{ marginLeft: '10px' }} onClick={handleUnsub}>
+                                        Unsubscribe
+                                    </Button>
+                                ) : null}
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </Card>
+            <div className="flex flex-wrap w-2/5 gap-6 text-xl bg-white mb-5 px-6 py-6">
+                <div className="w-full border-b-2 font-bold">Connection</div>
+                <div className="flex flex-col">
+                    <label htmlFor="host">Host</label>
+                    <input type="text" id="host" value={clientOptions.host} className="border border-gray-300 px-3" />
+                </div>
+            </div>
+        </div>
     );
-
-    return <Card title="Subscriber">{SubForm}</Card>;
 };
